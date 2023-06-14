a57f2d8d854269da77c84b5a3b8603bd29c97c1f
Aakash Agrawal <aakash.agrawal@dish.com>
2023-06-14 00:46:53 +0000

    add function to convert tsdb from s3 to json

---
+++ tsdb_to_json_converter.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"context"
+    "encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"os"
+    "path/filepath"
+	"strings"
+
+	"github.com/ryotarai/prometheus-tsdb-dump/pkg/writer"
+
+	gokitlog "github.com/go-kit/kit/log"
+	"github.com/pkg/errors"
+	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/prometheus/prometheus/tsdb"
+	"github.com/prometheus/prometheus/tsdb/chunkenc"
+	"github.com/prometheus/prometheus/tsdb/chunks"
+    "github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+// Downloads a tsdb block from s3 and converts it to json
+//
+// Usage:
+//    go run tsdb_to_json_converter.go -bucket <> -prefix <> -local-path <> -block <> > <filepath_of_converted_json.json>
+
+var Bucket string
+var Prefix string
+var LocalDirectory string
+
+func main() {
+    flag.StringVar(&Bucket, "bucket", os.Getenv("S3_BUCKET"), "s3 bucket")
+	flag.StringVar(&Prefix, "prefix", os.Getenv("S3_REGION"), "aws region")
+	flag.StringVar(&LocalDirectory, "local-path", os.Getenv("S3_OBJECT_PATH"), "object path (w/o bucket)")
+    blockPath := flag.String("block", "", "Path to block directory")
+	labelKey := flag.String("label-key", "", "")
+	labelValue := flag.String("label-value", "", "")
+	externalLabels := flag.String("external-labels", "{}", "Labels to be added to dumped result in JSON")
+	minTimestamp := flag.Int64("min-timestamp", 0, "min of timestamp of datapoints to be dumped; unix time in msec")
+	maxTimestamp := flag.Int64("max-timestamp", math.MaxInt64, "min of timestamp of datapoints to be dumped; unix time in msec")
+	format := flag.String("format", "victoriametrics", "")
+	flag.Parse()
+
+    if Bucket == "" || Prefix == "" || LocalDirectory == "" ||*blockPath == "" {
+        flag.PrintDefaults()
+        log.Fatal(" required arguments missing")
+		os.Exit(1)
+	}
+	
+    // Load aws config anf download tsdb block
+    
+    cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	if err != nil {
+		log.Fatalln("error:", err)
+	}
+
+	client := s3.NewFromConfig(cfg)
+	manager := manager.NewDownloader(client)
+
+	paginator := s3.NewListObjectsV2Paginator(client, &s3.ListObjectsV2Input{
+		Bucket: &Bucket,
+		Prefix: &Prefix,
+	})
+
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(context.TODO())
+		if err != nil {
+			log.Fatalln("error:", err)
+		}
+		for _, obj := range page.Contents {
+			if err := downloadToFile(manager, LocalDirectory, Bucket, aws.ToString(obj.Key)); err != nil {
+				log.Fatalln("error:", err)
+			}
+		}
+	}
+    
+    
+    // Convert the downloaded tsdb block to json
+
+	if err := run(*blockPath, *labelKey, *labelValue, *format, *minTimestamp, *maxTimestamp, *externalLabels); err != nil {
+		log.Fatalf("error: %s", err)
+	}
+}
+
+
+func downloadToFile(downloader *manager.Downloader, targetDirectory, bucket, key string) error {
+	// Create the directories in the path
+	file := filepath.Join(targetDirectory, key)
+	if err := os.MkdirAll(filepath.Dir(file), 0775); err != nil {
+		return err
+	}
+
+	// Set up the local file
+	fd, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	// Download the file using the AWS SDK for Go
+	fmt.Printf("Downloading s3://%s/%s to %s...\n", bucket, key, file)
+	_, err = downloader.Download(context.TODO(), fd, &s3.GetObjectInput{Bucket: &bucket, Key: &key})
+
+	return err
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+    fmt.Fprintf(os.Stderr, msg+"\n", args...)
+    os.Exit(1)
+}
+
+func run(blockPath string, labelKey string, labelValue string, outFormat string, minTimestamp int64, maxTimestamp int64, externalLabelsJSON string) error {
+	externalLabelsMap := map[string]string{}
+	if err := json.NewDecoder(strings.NewReader(externalLabelsJSON)).Decode(&externalLabelsMap); err != nil {
+		return errors.Wrap(err, "decode external labels")
+	}
+	var externalLabels labels.Labels
+	for k, v := range externalLabelsMap {
+		externalLabels = append(externalLabels, labels.Label{Name: k, Value: v})
+	}
+
+	wr, err := writer.NewWriter(outFormat)
+
+	logger := gokitlog.NewLogfmtLogger(os.Stderr)
+
+	block, err := tsdb.OpenBlock(logger, blockPath, chunkenc.NewPool())
+	if err != nil {
+		return errors.Wrap(err, "tsdb.OpenBlock")
+	}
+
+	indexr, err := block.Index()
+	if err != nil {
+		return errors.Wrap(err, "block.Index")
+	}
+	defer indexr.Close()
+
+	chunkr, err := block.Chunks()
+	if err != nil {
+		return errors.Wrap(err, "block.Chunks")
+	}
+	defer chunkr.Close()
+
+	postings, err := indexr.Postings(labelKey, labelValue)
+	if err != nil {
+		return errors.Wrap(err, "indexr.Postings")
+	}
+
+	var it chunkenc.Iterator
+	for postings.Next() {
+		ref := postings.At()
+		lset := labels.Labels{}
+		chks := []chunks.Meta{}
+		if err := indexr.Series(ref, &lset, &chks); err != nil {
+			return errors.Wrap(err, "indexr.Series")
+		}
+		if len(externalLabels) > 0 {
+			lset = append(lset, externalLabels...)
+		}
+
+		for _, meta := range chks {
+			chunk, err := chunkr.Chunk(meta.Ref)
+			if err != nil {
+				return errors.Wrap(err, "chunkr.Chunk")
+			}
+
+			var timestamps []int64
+			var values []float64
+
+			it := chunk.Iterator(it)
+			for it.Next() {
+				t, v := it.At()
+				if math.IsNaN(v) {
+					continue
+				}
+				if math.IsInf(v, -1) || math.IsInf(v, 1) {
+					continue
+				}
+				if t < minTimestamp || maxTimestamp < t {
+					continue
+				}
+				timestamps = append(timestamps, t)
+				values = append(values, v)
+			}
+			if it.Err() != nil {
+				return errors.Wrap(err, "iterator.Err")
+			}
+
+			if len(timestamps) == 0 {
+				continue
+			}
+
+			if err := wr.Write(&lset, timestamps, values); err != nil {
+				return errors.Wrap(err, fmt.Sprintf("Writer.Write(%v, %v, %v)", lset, timestamps, values))
+			}
+		}
+	}
+
+	if postings.Err() != nil {
+		return errors.Wrap(err, "postings.Err")
+	}
+
+	return nil
+}
